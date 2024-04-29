1aed72b6a9763200ded3faa2f897fee910265baa
CRBroughton <crbroughton@posteo.uk>
2024-04-29 20:18:51 +0100

    feat: :sparkles: add basic movement

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ function _init()
     player = {}
     player.new = function ()
         local self = {
+            x = 0,
+            y = 0,
             currentSprite = 0,
             spriteLeft = 0,
             spriteRight = 0,
@@ -16,13 +18,25 @@ function _init()
         }
 
         function self.selectClass(className)
-            spr(self.classes.wizard.spriteLeft, 2, 3)
+            spr(self.classes.wizard.spriteLeft, self.x, self.y)
             self.spriteLeft = self.classes[className].spriteLeft
             self.spriteRight = self.classes[className].spriteRight
         end
 
         function self.draw() 
-           spr(self.classes.wizard.spriteLeft, 2, 3)
+           spr(self.classes.wizard.spriteLeft, self.x, self.y)
+        end
+
+        function self.move()
+            if btn(0) then
+                player.x -= 1
+            elseif btn(1) then
+                player.x += 1
+            elseif btn(2) then
+                player.y -= 1
+            elseif btn(3) then
+                player.y += 1
+            end
         end
 
         return self
@@ -32,6 +46,7 @@ function _init()
 end
 
 function _update60()
+    player.move()
 end
 
 function _draw()
