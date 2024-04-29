14bfe0c917d8473fb22e706f4dea8e3c0f7dbf16
CRBroughton <crbroughton@posteo.uk>
2024-04-29 20:03:39 +0100

    feat: :sparkles: setup project, can draw to screen

--- main.lua
+++ main.lua
@@ -1,4 +1,18 @@
 function _init()
+    local var screenWidth = 128
+    local var screenHeight = 128
+    player = {}
+    player.new = function ()
+        local self = {}
+
+        function self.draw() 
+           spr(0, 2, 3)
+        end
+
+        return self
+    end
+
+    test = player.new()
 end
 
 function _update60()
@@ -7,4 +21,5 @@ end
 function _draw()
     cls()
     rectfill(0, 0, 128, 128, 15)
+    test.draw()
 end
