22bd2e44a2443ab67e87e64a1f2c1e97dcc3d523
CRBroughton <crbroughton@posteo.uk>
2024-04-30 12:05:24 +0100

    feat: :sparkles: create beginnings of basic projectile system

--- main.lua
+++ main.lua
@@ -1,22 +1,92 @@
 function _init()
     local var screenWidth = 128
     local var screenHeight = 128
+    local var offScreen = -8
+
+    projectiles = {}
+    projectiles.new = function()
+        local self = {}
+
+        function self.draw()
+            for proj in all(projectiles) do
+                spr(proj.sprite, proj.x, proj.y)
+            end
+        end
+
+        function self.update()
+            for proj in all(projectiles) do
+                if proj.dir == 'left' then
+                    proj.x -= proj.speed
+                end
+                if proj.dir == 'right' then
+                    proj.x += proj.speed
+                end
+                delete(proj)
+            end
+        end
+
+        function delete(proj)
+            if proj.x > screenWidth then
+                del(projectiles, proj)
+            end
+            if proj.x < offScreen then
+                del(projectiles, proj)
+            end
+            if proj.y > screenHeight then
+                del(projectiles, proj)
+            end
+            if proj.y < offScreen then
+                del(projectiles, proj)
+            end
+        end
+
+        return self
+    end
+
     player = {}
     player.new = function ()
         local self = {
             x = 0,
             y = 0,
+            facing = 'right',
             currentSprite = 0,
             spriteLeft = 0,
             spriteRight = 0,
             classes = {
                 ['wizard'] = {
-                    spriteLeft = 0,
-                    spriteRight = 1
+                    spriteLeft = 1,
+                    spriteRight = 0
+                }
+            },
+            attacks = {
+                ['fireball'] = {
+                    sprite = 2,
+                    power = 1,
                 }
             }
         }
 
+        function self.attack()
+            if self.facing == 'left' and btnp(5) then
+                add(projectiles, {
+                    x = self.x - 8,
+                    y = self.y,
+                    speed = 1,
+                    dir = 'left',
+                    sprite = self.attacks.fireball.sprite
+                })
+            end
+            if self.facing == 'right' and btnp(5) then
+                add(projectiles, {
+                    x = self.x + 8,
+                    y = self.y,
+                    speed = 1,
+                    dir = 'right',
+                    sprite = self.attacks.fireball.sprite
+                })
+            end
+        end
+    
         function self.selectClass(className)
             spr(self.classes.wizard.spriteLeft, self.x, self.y)
             self.spriteLeft = self.classes[className].spriteLeft
@@ -31,10 +101,12 @@ function _init()
         function self.move()
             if btn(0) then
                 player.x -= 1
-                self.currentSprite = self.spriteRight
+                self.currentSprite = self.spriteLeft
+                self.facing = 'left'
             elseif btn(1) then
                 player.x += 1
-                self.currentSprite = self.spriteLeft
+                self.currentSprite = self.spriteRight
+                self.facing = 'right'
             elseif btn(2) then
                 player.y -= 1
             elseif btn(3) then
@@ -46,14 +118,18 @@ function _init()
     end
 
     player = player.new()
+    projectiles = projectiles.new()
 end
 
 function _update60()
     player.move()
+    projectiles.update()
 end
 
 function _draw()
     cls()
     player.selectClass("wizard")
     player.draw()
+    player.attack()
+    projectiles.draw()
 end
